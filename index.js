d437050ec7d9cd1848122dfc76f2066dca75d644
shadrach-se <shadrach.se@gmail.com>
2024-03-22 03:59:10 +0100

    feat: revert origin whiteList

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors_proxy = require("cors-anywhere");
 const port = env.PORT || 8080;
 cors_proxy
   .createServer({
-    originWhitelist: ["*"], // Allow all origins
+    originWhitelist: [], // Allow all origins
     requireHeader: ["origin", "x-requested-with"],
     removeHeaders: ["cookie", "cookie2"],
   })
