0f548fe6b47ff3387b5fe25df6c6ae446e26e65f
shadrach-se <shadrach.se@gmail.com>
2024-03-22 03:46:45 +0100

    feat: initial setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const { parsed: env } = require("dotenv").config();
+const cors_proxy = require("cors-anywhere");
+
+const port = env.PORT || 8080;
+cors_proxy
+  .createServer({
+    originWhitelist: ["*"], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, function () {
+    console.log(`Server is listening on port ${env.PORT}... `);
+  });
